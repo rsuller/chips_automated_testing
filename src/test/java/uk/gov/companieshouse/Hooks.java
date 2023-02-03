e1bf318517cf27819f6c54357090d35062caa571
Daniel Falconer <dfalconer@companieshouse.gov.uk>
2023-02-03 11:01:36 +0000

    add and call powershell script to disable browser sign in

--- src/test/java/uk/gov/companieshouse/Hooks.java
+++ src/test/java/uk/gov/companieshouse/Hooks.java
@@ -1,10 +1,22 @@
 package uk.gov.companieshouse;
 
+import com.profesorfalken.jpowershell.PowerShell;
+import com.profesorfalken.jpowershell.PowerShellNotAvailableException;
+import com.profesorfalken.jpowershell.PowerShellResponse;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Objects;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import uk.gov.companieshouse.utils.TestContext;
 
 public class Hooks {
+
+    public static final Logger log = LoggerFactory.getLogger(Hooks.class);
     public TestContext testContext;
 
     public Hooks(TestContext testContext) {
@@ -13,8 +25,20 @@ public class Hooks {
 
     @Before
     public void setUp() {
+
+        try (PowerShell powerShell = PowerShell.openSession()) {
+            String script = "/browserSignIn.ps1";
+            //Read the resource
+            BufferedReader srcReader = new BufferedReader(
+                    new InputStreamReader(Objects.requireNonNull(getClass().getResourceAsStream(script))));
+            PowerShellResponse response = powerShell.executeScript(srcReader);
+            log.info("Powershell script executed successfully");
+        } catch (PowerShellNotAvailableException ex) {
+            throw new RuntimeException("Powershell not available");
+        }
         testContext.getWebDriver().get(
                 testContext.getEnv().config.getString("chips_url"));
+
     }
 
     @After
