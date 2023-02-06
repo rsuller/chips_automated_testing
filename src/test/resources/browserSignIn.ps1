e8a690aaa2f633187ad5d7cd40d507ddde74abe0
danfalc <dfalconer@companieshouse.gov.uk>
2023-02-06 13:08:00 +0000

    Update browserSignIn.ps1

--- src/test/resources/browserSignIn.ps1
+++ src/test/resources/browserSignIn.ps1
@@ -4,7 +4,7 @@ param (
     [string]
     $browserSignInValue
 )
-if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit }
+if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process powershell.exe "-NoProfile -NonInteractive -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit }
 
 
 
