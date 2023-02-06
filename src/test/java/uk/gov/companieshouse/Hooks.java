756fc6fdcebf41c7c8da028483d8fe05a693090a
Daniel Falconer <dfalconer@companieshouse.gov.uk>
2023-02-06 15:33:52 +0000

    remove unused logger

--- src/test/java/uk/gov/companieshouse/Hooks.java
+++ src/test/java/uk/gov/companieshouse/Hooks.java
@@ -2,13 +2,10 @@ package uk.gov.companieshouse;
 
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import uk.gov.companieshouse.utils.TestContext;
 
 public class Hooks {
 
-    public static final Logger log = LoggerFactory.getLogger(Hooks.class);
     public TestContext testContext;
 
     public Hooks(TestContext testContext) {
