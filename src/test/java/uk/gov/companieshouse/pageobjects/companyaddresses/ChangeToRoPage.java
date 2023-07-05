732002faad57076e583269a363eddde1711269ca
Daniel Falconer <dfalconer@companieshouse.gov.uk>
2023-07-05 11:36:54 +0100

    Add missing import

--- src/test/java/uk/gov/companieshouse/pageobjects/companyaddresses/ChangeToRoPage.java
+++ src/test/java/uk/gov/companieshouse/pageobjects/companyaddresses/ChangeToRoPage.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.support.How;
 import org.openqa.selenium.support.PageFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import uk.gov.companieshouse.pageobjects.AddressPageInterface;
 import uk.gov.companieshouse.pageobjects.ChipsCommonPage;
 import uk.gov.companieshouse.utils.TestContext;
 
